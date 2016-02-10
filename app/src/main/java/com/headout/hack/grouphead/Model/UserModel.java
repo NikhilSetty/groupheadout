e1049f267f857c50ae4afb2127bdc91b8796c224
NikhilSetty <nikhilsettyr@gmail.com>
2016-02-10 14:44:21 +0530

    Refactored.

--- app/src/main/java/com/headout/hack/grouphead/Model/UserModel.java
+++
@@ -1,6 +0,0 @@
-package com.headout.hack.grouphead.Model;
-
-public class UserModel {
-    public String UserName;
-    public long Password;
-}
--- app/src/main/java/com/headout/hack/grouphead/StaticHelper.java
+++
@@ -1,6 +0,0 @@
-package com.headout.hack.grouphead;
-
-public class StaticHelper {
-
-    public static boolean IsFromSignUp = false;
-}
